d4f481a0c10349753912f83fcc1d1950b2dfa5a3
12816 <aybolek27@gmail.com>
2022-10-27 22:43:22 -0500

    database connection - H2 database testing purpose

--- Spring-09-Spring-Thymeleaf/src/main/java/com/domain/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/domain/controller/StudentController.java
@@ -4,14 +4,16 @@ package com.domain.controller;
 import com.domain.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register")
+//    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    @GetMapping("/register")
     public String register(Model model){
 
         model.addAttribute("students", DataGenerator.createStudent());
@@ -20,9 +22,9 @@ public class StudentController {
 
 
 
-    @RequestMapping("/welcome")
+//    @RequestMapping("/welcome")
+    @GetMapping("/welcome")
     public String info(){
-
         return "/student/welcome";
     }
 
